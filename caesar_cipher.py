b1dcd3b48023f86beb371f391d5aedc5dc4a0188
Jakobie <k0bie11@yahoo.com>
2025-09-21 14:29:53 -0300

    Final Caesar Cipher code challenge

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,25 +1,34 @@
-from tokenize import endpats
-from wsgiref.util import shift_path_info
-
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-# Create a function for encrypt()
-def encrypt(original_text, shift_amount):
-    encoded_result = ""
+def caesar(original_text, shift_amount, encode_or_decode):
+    text_result = ''
     
-    # for each letter in the original text, find its index in the alphabet and change it to index + shift
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
     for letter in original_text:
-        new_index = (alphabet.index(letter) + shift) % 26
-        encoded_result += alphabet[new_index]
+        if letter in alphabet: 
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            text_result += alphabet[shifted_position]
+        elif letter not in alphabet:
+            text_result += letter
 
-    print(encoded_result)
+    print(f"Here is the {direction}d result: {text_result}")
 
-# When use inputs encode, use the encypt function
-if direction == "encode":
-    encrypt(original_text=text, shift_amount=shift)
+go_again = True
+
+while go_again:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    continue_cipher = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n" )
+
+    if continue_cipher == 'no':
+        print("Goodbye!")
+        go_again = False
